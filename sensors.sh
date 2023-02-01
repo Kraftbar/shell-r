39892a1534d93008499bcc21128216b660f57e78
Gaute <gautenybo@gmail.com>
2023-02-01 18:42:48 +0100

    Create sensors.sh

---
+++ sensors.sh
@@ -0,0 +1,4 @@
+upower -i $(upower -e | grep 'BAT') | grep --color=never -E percentage | awk '{print $2}'
+echo "Wi-Fi Signal Strength: $(iwconfig 2>&1 | awk -F '[ =]+' '/Quality/ {print $4}')"
+iwconfig 2>&1 | grep "Bit Rate"  |  awk '{print $1 " " $2 " " $3}'
+ip addr | awk ' /(inet)(.*)brd/ {print $2 }'
