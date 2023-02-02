b3817a83b0362cd1f16922f09938cc354b17719c
Gaute <gautenybo@gmail.com>
2023-02-02 18:09:39 +0100

    Update sensors.sh

--- sensors.sh
+++ sensors.sh
@@ -2,3 +2,4 @@ upower -i $(upower -e | grep 'BAT') | grep --color=never -E percentage | awk '{p
 echo "Wi-Fi Signal Strength: $(iwconfig 2>&1 | awk -F '[ =]+' '/Quality/ {print $4}')"
 iwconfig 2>&1 | grep "Bit Rate"  |  awk '{print $1 " " $2 " " $3}'
 ip addr | awk ' /(inet)(.*)brd/ {print $2 }'
+iwlist scan 2>&1 | grep -A22 -B5 -i "por "
