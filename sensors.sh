da304e77a0b0e3ec39beab3918726b1a7d1a1ed0
Gaute <gautenybo@gmail.com>
2023-02-11 13:36:14 +0100

    Update sensors.sh

--- sensors.sh
+++ sensors.sh
@@ -3,3 +3,8 @@ echo "Wi-Fi Signal Strength: $(iwconfig 2>&1 | awk -F '[ =]+' '/Quality/ {print 
 iwconfig 2>&1 | grep "Bit Rate"  |  awk '{print $1 " " $2 " " $3}'
 ip addr | awk ' /(inet)(.*)brd/ {print $2 }'
 iwlist scan 2>&1 | grep -A22 -B5 -i "por "
+
+iw dev wlp2s0 info | awk '/width/ {print "Width " $6 "MHz"}'
+iw dev wlp2s0 info | awk '/channel/ {print "Channel " $2}'
+iw dev wlp2s0 info | awk '/channel/ {print "Center " $3 }' | sed 's/(//'
+iw dev wlp2s0 info | awk '/channel/ {print "Center " $9 }' 
